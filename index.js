febe50411847e80e8b3b723338583484144ccf5f
Spencer <spencer.adler2@gmail.com>
2024-04-18 18:12:30 -0400

    Complete Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+// Write your solution in this file
+const employee = {
+    name: "Spencer",
+    streetAddress: "2 Mount Joy",
+  };
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    const employees = { ...employee };
+  
+    employees[key] = value;
+  
+    return  employees ;
+  }
+ 
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  
+    employee[key] = value;
+  
+    return  employee ;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    
+    const employees = { ...employee };
+
+    delete employees.name;
+  
+    return  employees ;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee.name;
+  
+    return  employee ;
+  }
